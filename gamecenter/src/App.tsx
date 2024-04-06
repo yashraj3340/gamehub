86006fdf39f9bdb51617b687c2d7c07fae6e909f
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-06 15:24:24 +0530

    Display Genres

--- gamecenter/src/App.tsx
+++ gamecenter/src/App.tsx
@@ -14,12 +14,16 @@ const App = () => {
         "aside main"
       `,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>

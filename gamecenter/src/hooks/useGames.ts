939d96de1cb65dcc5b93734c4adaa008e91fe47a
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-13 15:15:06 +0530

    refactoring - fetch games with react query

--- gamecenter/src/hooks/useGames.ts
+++ gamecenter/src/hooks/useGames.ts
@@ -1,5 +1,7 @@
- import { GameQuery } from '../App';
-import useData from './useData';
+ import { useQuery } from '@tanstack/react-query';
+import { GameQuery } from '../App';
+import apiClient from '../services/api-client';
+import { FetchResponse } from '../services/api-client';
 
 
 export interface Platform {
@@ -18,9 +20,19 @@ export interface Game {
 }
 
 
-const useGames = (
-  (gameQuery:GameQuery) => useData<Game>("/games",
-  {params: {genres: gameQuery.genre?.id,platforms: gameQuery.platform?.id,ordering: gameQuery.sortOrder,search: gameQuery.searchText}},
-  [gameQuery])
-)
+const useGames = (gameQuery: GameQuery) => {
+  return useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Game>>('/games', {
+        params: {
+          genres: gameQuery.genre?.id,
+          platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      })
+      .then((response) => response.data),
+  });
+};
 export default useGames;

2a2c4fe96d6870cd34299d510914814d9a1858a0
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-13 14:49:13 +0530

    Changing platforms using react qery and react hooks

--- gamecenter/src/hooks/usePlatforms.ts
+++ gamecenter/src/hooks/usePlatforms.ts
@@ -1,13 +1,22 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
     id: number;
-    name: string;   
+    name: string;
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
-
-
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    initialData: { 
+        count: 0, results: [] 
+    }
+}); 
 
 export default usePlatforms;

fe1f5dc154a2d6fb338e59068ca8f5d4ff7456e4
Yashraj Gharte <bt22csh012@iiitn.ac.in>
2024-04-17 22:39:15 +0530

    refactoring-removing duplicated interfaces

--- gamecenter/src/hooks/useGames.ts
+++ gamecenter/src/hooks/useGames.ts
@@ -1,14 +1,8 @@
- import { useQuery } from '@tanstack/react-query';
-import { GameQuery } from '../App';
-import apiClient from '../services/api-client';
-import { FetchResponse } from '../services/api-client';
-
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { useQuery } from "@tanstack/react-query";
+import { GameQuery } from "../App";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
+import { Platform } from "./usePlatforms";
 
 export interface Game {
   id: number;
@@ -19,20 +13,20 @@ export interface Game {
   rating_top: number;
 }
 
-
 const useGames = (gameQuery: GameQuery) => {
   return useQuery<FetchResponse<Game>, Error>({
-    queryKey: ['games', gameQuery],
+    queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient.get<FetchResponse<Game>>('/games', {
-        params: {
-          genres: gameQuery.genre?.id,
-          platforms: gameQuery.platform?.id,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-        },
-      })
-      .then((response) => response.data),
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((response) => response.data),
   });
 };
 export default useGames;
